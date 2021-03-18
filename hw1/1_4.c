49c88dfdae01768b0bac82e77fb619f2de48f243
dosonyueh <dosonyueh0418@gmail.com>
2021-03-18 21:04:31 +0800

    20210318.2104

--- hw1/1_4.c
+++ hw1/1_4.c
@@ -10,26 +10,37 @@ int main()
 	int row = 0;
 	int i,j;
 	Input(input,length,&row);
-	for(i = 0 ; i<row;i++)
+/*	for(i = 0 ; i<row;i++)
 	{
 		for(j = 0;j<length[i];j++)
 			printf("%c",input[i][j]);
 		printf("\n");
 	}
 	printf("\n");
-}
+*/}
 
 void Input(char input[][Size],int* length,int* row)
 {
 	int i = 0;
-	while(scanf("%s",input[i]) != EOF)
+/*	while(scanf("%s",input[i]) != EOF)
 	{
 //		scanf("%s",input);
-		printf("strlen = %ld\n",strlen(input[i]));
 		printf("i = %d\n",i);
 		length[i] = strlen(input[i]);
-		printf("last = %c\n",input[i][length[i-2]]);
+		for(i = 0 ; i<length[0];i++)
+			printf("input = %c",input[0][i]);
+		printf("last = %c\n",input[0][length[0]]);
+		if(input[0][length[0]] == '\0')
+			printf("end\n");
+		printf("strlen = %d\n",length[0]);
 		i++;
 	}
+*/	
+//	scanf("%s",input[i]);
+	while(fgets(input[i],100,stdin) != NULL){
+		length[i] = strlen(input[i]);
+		printf("length = %d\n",length[i]);
+		for(int j = 0;j<length[0];j++)
+			printf("input = %c",input[0][j]);
+	}
 }
-
