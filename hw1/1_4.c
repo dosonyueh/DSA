606efcb69f56b0bd927c8d641e8cdcdb0401d158
dosonyueh <dosonyueh0418@gmail.com>
2021-03-18 16:32:52 +0800

    20210318.1632

--- hw1/1_4.c
+++ hw1/1_4.c
@@ -2,22 +2,34 @@
 #include <stdio.h>
 #include <string.h>
 #define Size 100
-void Input(char*,int*);
+void Input(char input[][Size],int*,int *);
 int main()
 {
-	char input[Size] = {0};
-	int length = 0;
-	int i;
-	Input(input,&length);
-	for(i = 0 ; i<length;i++)
-		printf("%c",input[i]);
+	char input[][Size] = {0};
+	int length[100] = {0};
+	int row = 0;
+	int i,j;
+	Input(input,length,&row);
+	for(i = 0 ; i<row;i++)
+	{
+		for(j = 0;j<length[i];j++)
+			printf("%c",input[i][j]);
+		printf("\n");
+	}
 	printf("\n");
 }
 
-void Input(char* input,int* length)
+void Input(char input[][Size],int* length,int* row)
 {
-	scanf("%s",input);
-	printf("strlen = %ld\n",strlen(input));
-	*length = strlen(input);
+	int i = 0;
+	while(scanf("%s",input[i]) != EOF)
+	{
+//		scanf("%s",input);
+		printf("strlen = %ld\n",strlen(input[i]));
+		printf("i = %d\n",i);
+		length[i] = strlen(input[i]);
+		printf("last = %c\n",input[i][length[i-2]]);
+		i++;
+	}
 }
 
