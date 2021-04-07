5b4664f144fbf3b4837ec38e75c07ebbfe8c919f
dosonyueh <dosonyueh0418@gmail.com>
2021-04-07 22:26:51 +0800

    123

--- hw1/1_6.c
+++ hw1/1_6.c
@@ -16,13 +16,36 @@ struct upper_node{
 };
 typedef struct upper_node U_Node;
 
-int num[NumofNode];
+int number[NumofNode] = {0};
 
-//void insert(Node** n_stack,int num,int numth,int )
+void Query(int start,int end,int size,U_Node** u_stack)
+{
+	int start_node = 0,end_node = 0,i = 0,num = 0;
+	int* seq = (int*)malloc(sizeof(int) * (end-start+1));
+	while(num < start)
+		num += number[start_node++];
+	start_node--;
+	num -= number[start_node];//此時num為第幾個數字
+	start -= num;
+	end -= num;
+	//end_node = start_node;
+	//while(num < end)
+	//	num += number[end_node++];
+	//end_node--;
+	Node* current = (Node*)malloc(sizeof(Node));
+	current = u_stack[start_node]->first_node;
+	for(i = 0;i<start-1;i++)
+		current = current->next;
+	for(i = 0;i<(end-start+1);i++)
+	{
+		seq[i] = current->element;
+		current = current->next;
+	}
+}
 
 void init(int* initial,int n,Node** n_stack,int* numofnode,U_Node** u_stack)
 {
-	int in_th = 0,number = 0,i,j = 0;
+	int i,j = 0;
 	U_Node *u_ptr = (U_Node*)malloc(sizeof(U_Node));
 	U_Node *u_current = (U_Node*)malloc(sizeof(U_Node));
 	Node *current = (Node*)malloc(sizeof(Node));
@@ -51,11 +74,14 @@ void init(int* initial,int n,Node** n_stack,int* numofnode,U_Node** u_stack)
 				u_current->next = u_ptr;
 			}
 			u_current = u_ptr;
-			u_stack[j] = u_ptr;
+			u_stack[j] = u_ptr; 
 			j++;
 		}
-		u_ptr->num++;
+		//(u_ptr->num) = u_ptr->num +1;
+		(u_stack[j-1]->num)++;
 	}
+	for(i = 0;i<j;i++)
+		number[i] = u_stack[i]->num;
 }
 
 int main()
@@ -73,6 +99,7 @@ int main()
 		scanf("%d",&initial[i]);
 	}
 	init(initial,n,n_stack,&numofnode,u_stack);
+	Query(4,11,1,u_stack);
 /*	for(i = 0;i<q;i++)
 	{
 		fgets(oper,50,stdin);
